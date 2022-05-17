67ea8dff9543ee6586c14c0da6f9967ee8000547
rachidassouani <rachidassouani@gmail.com>
2022-05-17 22:30:43 +0100

    Created the eureka-server module and added the spring cloud dependency as well as eureka server and client dependencies

--- fraud/src/main/java/io/rachidassouani/fraud/FraudCheckHistoryApplication.java
+++ fraud/src/main/java/io/rachidassouani/fraud/FraudCheckHistoryApplication.java
@@ -2,8 +2,10 @@ package io.rachidassouani.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FraudCheckHistoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(FraudCheckHistoryApplication.class, args);
